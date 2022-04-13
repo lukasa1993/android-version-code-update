27a22fbc8ebbd17c281181f5343fbadfe8cd4d3b
luka dodelia <luka@picktek.com>
2022-04-13 19:43:47 +0400

    correct version grab

--- src/gradle.ts
+++ src/gradle.ts
@@ -4,43 +4,43 @@ import * as core from "@actions/core";
 
 async function* walk(dir: string): AsyncIterable<string> {
   for await (const d of await fs.promises.opendir(dir)) {
-    const entry = path.join(dir, d.name)
-    if (d.isDirectory()) yield* walk(entry)
-    else if (d.isFile()) yield entry
+    const entry = path.join(dir, d.name);
+    if (d.isDirectory()) yield* walk(entry);
+    else if (d.isFile()) yield entry;
   }
 }
 
 async function getGradleFiles(): Promise<string[]> {
-  const paths: string[] = []
-  for (const p in walk('.')) {
-    const file = path.parse(p)
-    if (file.ext === '.gradle' || file.ext === '.kts') {
-      paths.push(p)
+  const paths: string[] = [];
+  for (const p in walk(".")) {
+    const file = path.parse(p);
+    if (file.ext === ".gradle" || file.ext === ".kts") {
+      paths.push(p);
     }
   }
-  return paths
+  return paths;
 }
 
 export async function UpdateVersionCode(versionCode: Number): Promise<void> {
   for (const file_path of await getGradleFiles()) {
     fs.readFile(file_path, (err, data) => {
-      if (err) core.error(err)
-      const lines = data.toString('utf-8').split(/\r?\n/)
+      if (err) core.error(err);
+      const lines = data.toString("utf-8").split(/\r?\n/);
       for (let index = 0; index < lines.length; index++) {
-        const line = lines[index]
-        if (line.includes('versionCode')) {
-          const re = /[\d]+/g
-          lines[index] = line.replace(re, String(versionCode))
+        const line = lines[index];
+        if (line.includes("versionCode")) {
+          const re = /[\d]+/g;
+          lines[index] = line.replace(re, String(versionCode));
         }
       }
-      const file = fs.createWriteStream(file_path)
-      file.on('error', function (error) {
-        core.error(error)
-      })
+      const file = fs.createWriteStream(file_path);
+      file.on("error", function(error) {
+        core.error(error);
+      });
       for (const v of lines) {
-        file.write(`${v}\n`)
+        file.write(`${v}\n`);
       }
-      file.end()
-    })
+      file.end();
+    });
   }
 }
--- src/publish.ts
+++ src/publish.ts
@@ -1,42 +1,33 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import {google, androidpublisher_v3} from 'googleapis'
-
-import {Compute} from 'google-auth-library/build/src/auth/computeclient'
-import {JWT} from 'google-auth-library/build/src/auth/jwtclient'
-import {UserRefreshClient} from 'google-auth-library/build/src/auth/refreshclient'
+import { androidpublisher_v3, google } from "googleapis";
 
 const androidPublisher: androidpublisher_v3.Androidpublisher = google.androidpublisher(
-  'v3'
-)
+  "v3"
+);
 
 export interface Options {
-  auth: any
-  applicationId: string
+  auth: any;
+  applicationId: string;
 }
 
 export async function fetchVersionCode(options: Options): Promise<Number> {
   const appEdit = await androidPublisher.edits.insert({
     packageName: options.applicationId,
     auth: options.auth
-  })
+  });
 
-  const tracks = await androidPublisher.edits.tracks.list({
+  const lists = await androidPublisher.edits.bundles.list({
     auth: options.auth,
     packageName: options.applicationId,
     editId: appEdit.data.id!
-  })
+  });
 
-  const versionCodes: number[] = []
-
-  tracks.data.tracks?.forEach(track => {
-    track.releases?.forEach(release => {
-      versionCodes.push(Number(release.versionCodes![0]))
-    })
-  })
-
-  if (versionCodes.length > 0) {
-    return Math.max(...versionCodes)
-  } else {
-    return 1
+  let versionCode: number = 1;
+  try {
+    versionCode = lists.data.bundles![lists.data.bundles!.length - 1].versionCode || 1;
+  } catch (e) {
   }
+
+
+  return versionCode;
 }
